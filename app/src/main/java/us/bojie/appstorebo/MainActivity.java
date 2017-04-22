9ced4852641027190d1958ea9fc9e1c0c2498bee
Bojie Jiang <jiangbojie@gmail.com>
2017-04-21 21:19:44 -0700

    Setup and navigationView

---
+++ app/src/main/java/us/bojie/appstorebo/MainActivity.java
@@ -0,0 +1,79 @@
+package us.bojie.appstorebo;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.drawer_layout)
+    DrawerLayout mDrawerLayout;
+    @BindView(R.id.navigation_view)
+    NavigationView mNavigationView;
+
+    private View headerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mDrawerLayout.addDrawerListener(new DrawerLayout.DrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+
+            }
+
+            @Override
+            public void onDrawerOpened(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+
+            }
+
+            @Override
+            public void onDrawerStateChanged(int newState) {
+
+            }
+        });
+
+        headerView = mNavigationView.getHeaderView(0);
+        headerView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "headerview clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.menu_app_update:
+                        Toast.makeText(MainActivity.this, "clicked app update", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_message:
+                        Toast.makeText(MainActivity.this, "clicked message", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.menu_setting:
+                        Toast.makeText(MainActivity.this, "clicked setting", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+
+                return false;
+            }
+        });
+    }
+}
