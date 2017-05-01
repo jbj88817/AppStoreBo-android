f6bff1542f29a1dbbb7d385acb64e93c603888af
Bojie Jiang <jiangbojie@gmail.com>
2017-04-30 17:32:09 -0700

    Fixed publicParams bug

--- app/src/main/java/us/bojie/appstorebo/common/http/CommonparamsInterceptor.java
+++ app/src/main/java/us/bojie/appstorebo/common/http/CommonparamsInterceptor.java
@@ -6,6 +6,8 @@ import com.google.gson.Gson;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 import okhttp3.HttpUrl;
 import okhttp3.Interceptor;
@@ -48,14 +50,34 @@ public class CommonparamsInterceptor implements Interceptor {
 
         if (method.equals("GET")) {
             HttpUrl httpUrl = request.url();
-            String oldParamJson = httpUrl.queryParameter(Constant.PARAM);
+            HashMap<String, Object> rootMap = new HashMap<>();
+            Set<String> paramNames = httpUrl.queryParameterNames();
+            for (String key : paramNames) {
+                if (Constant.PARAM.equals(key)) {
+                    String oldParamsJson = httpUrl.queryParameter(Constant.PARAM);
+                    if (oldParamsJson != null) {
+                        HashMap<String, Object> p = mGson.fromJson(oldParamsJson, HashMap.class); // origin params
+                        if (p != null) {
+                            for (Map.Entry<String, Object> entry : p.entrySet()) {
+                                rootMap.put(entry.getKey(), entry.getValue());
+                            }
+                        }
+                    }
+                } else {
+                    rootMap.put(key, httpUrl.queryParameter(key));
+                }
+            }
 
-            HashMap<String, Object> rootMap = mGson.fromJson(oldParamJson, HashMap.class); // origin params
             rootMap.put("publicParams", commonParamsMap);
             String newJsonParams = mGson.toJson(rootMap);
 
             String url = httpUrl.toString();
-            url = url.substring(0, url.indexOf("?")) + "?" + Constant.PARAM + "=" + newJsonParams;
+
+            int index = url.indexOf("?");
+            if (index > 0) {
+                url = url.substring(0, index);
+            }
+            url = url + "?" + Constant.PARAM + "=" + newJsonParams;
 
             request = request.newBuilder().url(url).build();
 
