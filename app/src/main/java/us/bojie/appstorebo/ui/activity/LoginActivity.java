b6157d3bec3f65ebf8d33108c34a300c04840292
Bojie Jiang <jiangbojie@gmail.com>
2017-05-02 21:55:55 -0700

    LoginActivity

--- app/src/main/java/us/bojie/appstorebo/ui/activity/LoginActivity.java
+++ app/src/main/java/us/bojie/appstorebo/ui/activity/LoginActivity.java
@@ -1,16 +1,88 @@
 package us.bojie.appstorebo.ui.activity;
 
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.widget.Toolbar;
+import android.widget.EditText;
 
+import com.jakewharton.rxbinding2.view.RxView;
+import com.jakewharton.rxbinding2.widget.RxTextView;
+
+import butterknife.BindView;
+import io.reactivex.Observable;
+import io.reactivex.functions.BiFunction;
+import io.reactivex.functions.Consumer;
 import us.bojie.appstorebo.R;
+import us.bojie.appstorebo.di.component.AppComponent;
+import us.bojie.appstorebo.ui.widget.LoadingButton;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends BaseActivity {
+
+
+    @BindView(R.id.tool_bar)
+    Toolbar mToolBar;
+    @BindView(R.id.txt_mobi)
+    EditText mTxtMobi;
+    @BindView(R.id.view_mobi_wrapper)
+    TextInputLayout mViewMobiWrapper;
+    @BindView(R.id.txt_password)
+    EditText mTxtPassword;
+    @BindView(R.id.view_password_wrapper)
+    TextInputLayout mViewPasswordWrapper;
+    @BindView(R.id.btn_login)
+    LoadingButton mBtnLogin;
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+    public int setLayout() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    public void setupActivityComponent(AppComponent appComponent) {
+
+    }
+
+    @Override
+    public void init() {
+        initView();
+    }
+
+    private void initView() {
+        Observable<CharSequence> mobileObservable = RxTextView.textChanges(mTxtMobi);
+        Observable<CharSequence> passwordObservable = RxTextView.textChanges(mTxtPassword);
+
+        Observable.combineLatest(mobileObservable, passwordObservable, new BiFunction<CharSequence, CharSequence, Boolean>() {
+            @Override
+            public Boolean apply(CharSequence mobi, CharSequence pwd) throws Exception {
+                return isPhoneValid(mobi.toString()) && isPasswordValid(pwd.toString());
+            }
+        }).subscribe(new Consumer<Boolean>() {
+            @Override
+            public void accept(Boolean aBoolean) throws Exception {
+                RxView.enabled(mBtnLogin).accept(aBoolean);
+            }
+        });
+
+
+        RxView.clicks(mBtnLogin).subscribe(new Consumer<Object>() {
+            @Override
+            public void accept(Object o) throws Exception {
+                if (!mTxtMobi.toString().trim().equals("13800138000")) {
+                    mViewMobiWrapper.setError("Wrong number");
+                } else {
+                    mViewMobiWrapper.setError("");
+                    mViewMobiWrapper.setErrorEnabled(false);
+                }
+            }
+        });
+    }
+
+
+
+    private boolean isPhoneValid(String phone) {
+        return phone.length() == 11;
+    }
+
+    private boolean isPasswordValid(String password) {
+        return password.length() >= 6;
     }
 }
