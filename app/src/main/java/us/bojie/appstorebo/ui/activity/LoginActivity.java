d7433cc05cc8ca75a823dcc4571e9c1d47e815fe
Bojie Jiang <jiangbojie@gmail.com>
2017-05-02 20:42:52 -0700

    Login layout

--- app/src/main/java/us/bojie/appstorebo/ui/activity/LoginActivity.java
+++ app/src/main/java/us/bojie/appstorebo/ui/activity/LoginActivity.java
@@ -1,60 +1,16 @@
 package us.bojie.appstorebo.ui.activity;
 
-import android.Manifest;
 import android.os.Bundle;
-import android.widget.Button;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
-import com.tbruyelle.rxpermissions2.RxPermissions;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
-import io.reactivex.functions.Consumer;
 import us.bojie.appstorebo.R;
-import us.bojie.appstorebo.di.component.AppComponent;
 
-public class LoginActivity extends BaseActivity {
-
-    private static final int READ_PHONE_STATE_CODE = 1000;
-    @BindView(R.id.button)
-    Button mButton;
+public class LoginActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ButterKnife.bind(this);
-    }
-
-    @Override
-    public int setLayout() {
-        return R.layout.activity_login;
-    }
-
-    @Override
-    public void setupActivityComponent(AppComponent appComponent) {
-
-    }
-
-    @Override
-    public void init() {
-
-    }
-
-    @OnClick(R.id.button)
-    public void onViewClicked() {
-
-        RxPermissions rxPermissions = new RxPermissions(this);
-
-        rxPermissions.request(Manifest.permission.CAMERA)
-                .subscribe(new Consumer<Boolean>() {
-                    @Override
-                    public void accept(Boolean aBoolean) throws Exception {
-                        if (aBoolean) {
-
-                        } else {
-
-                        }
-                    }
-                });
+        setContentView(R.layout.activity_login);
     }
 }
