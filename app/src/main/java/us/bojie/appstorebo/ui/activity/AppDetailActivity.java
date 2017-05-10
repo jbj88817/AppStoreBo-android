07296bc943a403a8e14fd51ef65e5d4cc521c574
Bojie Jiang <jiangbojie@gmail.com>
2017-05-09 22:18:57 -0700

    Adjust the view position

--- app/src/main/java/us/bojie/appstorebo/ui/activity/AppDetailActivity.java
+++ app/src/main/java/us/bojie/appstorebo/ui/activity/AppDetailActivity.java
@@ -2,10 +2,13 @@ package us.bojie.appstorebo.ui.activity;
 
 import android.graphics.Bitmap;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import butterknife.BindView;
 import us.bojie.appstorebo.R;
+import us.bojie.appstorebo.common.util.DensityUtil;
 import us.bojie.appstorebo.di.component.AppComponent;
 
 public class AppDetailActivity extends BaseActivity {
@@ -32,6 +35,23 @@ public class AppDetailActivity extends BaseActivity {
         if (bitmap != null) {
             mImgView.setImageBitmap(bitmap);
         }
+
+        int[] location = new int[2];
+        view.getLocationOnScreen(location);
+
+        int left = location[0];
+        int top = location[1];
+
+        ViewGroup.MarginLayoutParams marginLayoutParams =
+                new ViewGroup.MarginLayoutParams(mImgView.getLayoutParams());
+        marginLayoutParams.topMargin = top - DensityUtil.getStatusBarH(this);;
+        marginLayoutParams.leftMargin = left;
+        marginLayoutParams.width = view.getWidth();
+        marginLayoutParams.height = view.getHeight();
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(marginLayoutParams);
+
+        mImgView.setLayoutParams(params);
     }
 
     private Bitmap getViewImageCache(View view) {
